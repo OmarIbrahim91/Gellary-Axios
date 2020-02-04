305d87517ded74700bc7e031696ab063f53edcea
root <omarali43620@yahoo.com>
2020-02-04 13:48:21 +0100

    Updates

---
+++ precache-manifest.700dd2d79b926c4d888f89bb974ea317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9272f8fe6c3f86a8c80845c15906eabc",
+    "url": "/Gellary-Axios/index.html"
+  },
+  {
+    "revision": "1d53f8c5c3727a8adfb4",
+    "url": "/Gellary-Axios/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b8064bc544d20d6f4394",
+    "url": "/Gellary-Axios/static/js/2.0b98e3e6.chunk.js"
+  },
+  {
+    "revision": "1d53f8c5c3727a8adfb4",
+    "url": "/Gellary-Axios/static/js/main.96ca3e0d.chunk.js"
+  },
+  {
+    "revision": "898bffa8dd20f49e98c8",
+    "url": "/Gellary-Axios/static/js/runtime-main.6acd794e.js"
+  }
+]);
